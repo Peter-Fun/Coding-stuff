0ab9b7f24a2ae3a839d86249d86edcee216ab8a3
Peter-Fun <76695155+Peter-Fun@users.noreply.github.com>
2022-01-06 11:57:11 -0500

    Create LeetCode 1094 Car Pooling.py

---
+++ LeetCode 1094 Car Pooling.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def carPooling(self, trips, capacity):
+        """
+        :type trips: List[List[int]]
+        :type capacity: int
+        :rtype: bool
+        """
+        start_timings = sorted([(trip[1], trip[0]) for trip in trips])
+        end_timings = sorted([(trip[2], trip[0]) for trip in trips])
+        passengers = 0
+        start = end = 0
+        while(start < len(trips)):
+            if start_timings[start][0] < end_timings[end][0]:
+                passengers += start_timings[start][1]
+            else:
+                passengers -= end_timings[end][1]
+                end += 1
+                continue
+            start += 1
+            if passengers > capacity:
+                return False
+        return True
+        
