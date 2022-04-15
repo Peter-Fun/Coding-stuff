b7038e0ed7b493622733419938de9f90739ec406
Peter-Fun <76695155+Peter-Fun@users.noreply.github.com>
2022-04-15 09:11:08 -0400

    Create HackerRank Sherlock and Array.py

---
+++ HackerRank Sherlock and Array.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    prefix = [0]
+    for i in arr:
+        prefix.append(prefix[-1] + i)
+    del prefix[0]
+    for i in range(len(arr)):
+        if prefix[i] - arr[i] == prefix[-1] - prefix[i]:
+            return "YES"
+    return "NO"
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
