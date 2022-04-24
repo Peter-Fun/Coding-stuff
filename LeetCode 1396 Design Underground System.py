f0541d5b8729e6bf86fb7340b2ec6c2fe93bae51
Peter-Fun <76695155+Peter-Fun@users.noreply.github.com>
2022-04-24 08:01:41 -0400

    Create LeetCode 1396 Design Underground System.py

---
+++ LeetCode 1396 Design Underground System.py
@@ -0,0 +1,40 @@
+class UndergroundSystem(object):
+
+    def __init__(self):
+        self.user = collections.defaultdict(list)
+        self.dest = collections.defaultdict(list)
+
+    def checkIn(self, id, stationName, t):
+        """
+        :type id: int
+        :type stationName: str
+        :type t: int
+        :rtype: None
+        """
+        self.user[id] = [stationName, t]
+
+    def checkOut(self, id, stationName, t):
+        """
+        :type id: int
+        :type stationName: str
+        :type t: int
+        :rtype: None
+        """
+        start_station, prev_time = self.user[id]
+        self.dest[(start_station, stationName)].append(t-prev_time)
+
+    def getAverageTime(self, startStation, endStation):
+        """
+        :type startStation: str
+        :type endStation: str
+        :rtype: float
+        """
+        return float(sum(self.dest[(startStation,endStation)]))/len(self.dest[(startStation,endStation)])
+        
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
