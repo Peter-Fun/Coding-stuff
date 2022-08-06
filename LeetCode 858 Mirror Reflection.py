b2b9fac7d6da69aec387cabc10c4a7a5907f8539
Peter-Fun <76695155+Peter-Fun@users.noreply.github.com>
2022-08-06 15:47:04 -0400

    Create LeetCode 858 Mirror Reflection.py

---
+++ LeetCode 858 Mirror Reflection.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def mirrorReflection(self, p, q):
+        """
+        :type p: int
+        :type q: int
+        :rtype: int
+        """
+        while p % 2 == 0 and q % 2 == 0: p, q = p / 2, q / 2
+        return 1 - p % 2 + q % 2
