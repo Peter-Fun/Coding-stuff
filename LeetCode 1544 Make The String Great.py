83cbd7eeaf6b9130736221602e7c26c689521a56
Peter-Fun <76695155+Peter-Fun@users.noreply.github.com>
2022-11-08 10:30:41 -0500

    Create LeetCode 1544 Make The String Great.py

---
+++ LeetCode 1544 Make The String Great.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def makeGood(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        i = 0
+        while(i >= 0 and i < len(s)-1):
+            if s[i].lower() == s[i+1].lower() and ( (s[i].islower() and s[i+1].isupper()) or (s[i].isupper() and s[i+1].islower()) ):
+                s = s[:i] + s[i+2:]
+                i -= 1
+            else:
+                i += 1
+            if i < 0:
+                i = 0
+        return s
