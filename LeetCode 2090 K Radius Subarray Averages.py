2850d530c818033c20f343406d2f0098e4e382a0
Peter-Fun <76695155+Peter-Fun@users.noreply.github.com>
2023-06-20 07:49:50 -0400

    Create LeetCode 2090 K Radius Subarray Averages.py

---
+++ LeetCode 2090 K Radius Subarray Averages.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def getAverages(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        windowSize = 2 * k + 1
+
+        windowSum = 0
+        result = [-1] * n
+
+        if n < windowSize:
+            return result
+
+        for i in range(n):
+            windowSum += nums[i]  # Add nums[i] to the window sum
+
+            if i - windowSize >= 0:
+                windowSum -= nums[i - windowSize]  # Remove nums[i - windowSize] from the window sum
+
+            if i >= windowSize - 1:
+                result[i - k] = windowSum // windowSize  # Calculate and store the average in the result
+
+        return result
